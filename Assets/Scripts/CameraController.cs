cc1632fba5516fd3eeec991cb76357e7c892083b
Lee Tze Cheun <leetzecheun@gmail.com>
2014-02-14 11:35:41 +0800

    First commit.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	/// <summary>
+	/// Reference to the Player object in the Unity scene.
+	/// </summary>
+	public GameObject player;
+
+	/// <summary>
+	/// The Camera's relative position to the Player object.
+	/// </summary>
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start() 
+	{
+		// Initially offset is the camera's original position.
+		offset = transform.position;
+	}
+	
+	// A follow camera should only be implemented in a LateUpdate method,
+	// according to Unity docs.
+	void LateUpdate() 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	/// <summary>
+	/// The Player's speed. This value will be modified from Unity's Inspector panel.
+	/// </summary>
+	public float speed;
+
+	/// <summary>
+	/// Reference to the GUIText component that will display the player's score.
+	/// </summary>
+	public GUIText countText;
+
+	/// <summary>
+	/// Reference to the GUIText component that will display when the player has
+	/// collected all the pickups.
+	/// </summary>
+	public GUIText winText;
+
+	/// <summary>
+	/// The number of Pickup objects that Player has collected.
+	/// </summary>
+	private int pickupCount = 0;
+
+	void Start()
+	{
+		SetPickupCountAndUpdateGUIText(0);
+
+		// The win text should not be visible until the Player has won.
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		// Y-Axis is 0 because the Player cannot move up.
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}	
+
+	void OnTriggerEnter(Collider other) 
+	{
+		// Note: We can use == to compare string values in C# because of the 
+		// overloaded operator.
+
+		// Deactivate the Pickup object when Player collects it.
+		if (other.gameObject.tag == "Pickup") 
+		{
+			other.gameObject.SetActive(false);
+			SetPickupCountAndUpdateGUIText(pickupCount + 1);
+		}
+	}
+
+	/// <summary>
+	/// Sets the pickup count property and updates the GUIText component also.
+	/// </summary>
+	/// <param name="count">The updated pickup count.</param>
+	void SetPickupCountAndUpdateGUIText(int count)
+	{
+		pickupCount = count;
+		countText.text = string.Format("Count: {0}", pickupCount);
+
+		if (pickupCount >= 12) 
+		{
+			winText.text = "Yay! You've won. :-)";
+		}
+	}
+}
